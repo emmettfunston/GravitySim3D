91937b9c396e094f76c303e00b1a3e195f29db50
Emmett Funston <emmettfunstuff@gmail.com>
2025-07-24 16:43:44 -0700

    GLUtilities

---
+++ src/GLUtilities.cpp
@@ -0,0 +1,33 @@
+#include "GLUtilities.hpp"
+#include <iostream>
+
+bool initWindow(int width, int height, const char* title, GLFWwindow*& window) {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return false;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return false;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return false;
+    }
+    glEnable(GL_DEPTH_TEST);
+    return true;
+}
+
+void cleanupWindow(GLFWwindow* window) {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
